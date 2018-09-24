c86d16b72d47da81cbf9d83e957f9b0e2cf9a1dd
Mayukh Chatterjee <mayukh.chatt@gmail.com>
2018-09-24 23:22:41 +0530

    Add files via upload

---
+++ CN_STL_2.cpp
@@ -0,0 +1,43 @@
+#include<cstring>
+#include<map>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  string s;
+  int i,flag=0;
+  map<char,int> m;
+
+  cin>>s;
+  
+  for(i=0;i<s.length();i++)
+    m[s[i]]=m[s[i]]+1;
+
+    map<char,int>::iterator it;
+
+    for(it=m.begin();it!=m.end();it++)
+    cout<<it->first <<" : " <<it->second <<endl;
+    
+    for(i=0;i<s.length();i++)
+    {   
+      for(it=m.begin();it!=m.end();it++)
+        if(s[i]==it->first&&it->second==1)
+         {  
+          cout<<it->first;
+          flag=1;
+         }
+        if(flag==1)
+          break;
+    }
+
+    if(flag==0)
+      {
+        i=0;
+        cout<<s[i];
+      }
+
+
+  return 0;
+}
